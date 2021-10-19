02ace6c1e2716309149a23764abf3b117078add2
Jankowski Łukasz <jankowski.lukasz0987@gmail.com>
2021-10-19 20:12:44 +0200

    Add files via upload

---
+++ Po1.java
@@ -0,0 +1,70 @@
+package po1;
+
+public class Po1 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------#1----------------------------------------");
+        System.out.println(31 + 29 + 31); //1
+        System.out.println("---------------------------#2----------------------------------------");
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10); //2
+        System.out.println("---------------------------#3----------------------------------------");
+        System.out.println(2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10); //3
+        System.out.println("---------------------------#4----------------------------------------");
+        System.out.println(1000 * 1.06 * 1.06 * 1.06); //4
+        System.out.println("---------------------------#5----------------------------------------");
+        System.out.println("""
+                +----+
+                |Java|
+                +----+"""); //5
+        System.out.println("---------------------------#6----------------------------------------");
+        System.out.println("""
+                  ///// \s
+                 +""\"""+\s
+                (| o o |)
+                  | ^ | \s
+                 | \u060C_\u2E34 |\s
+                 +-----+"""); //6
+        System.out.println("---------------------------#10----------------------------------------");
+        System.out.println("""
+                1.Jan Kowalski
+                2.Jan Kowalski
+                3.Jan Kowalski"""); //10
+        System.out.println("---------------------------#11----------------------------------------");
+        System.out.println("""
+                Niektórzy –
+                czyli nie wszyscy.
+                Nawet nie większość wszystkich, ale mniejszość.
+                Nie licząc szkół, gdzie się musi,
+                i samych poetów,
+                będzie tych osób chyba dwie na tysiąc.
+
+                Lubią –
+                ale lubi się także rosół z makaronem,
+                lubi się komplementy i kolor niebieski,
+                lubi się stary szalik,
+                lubi się stawiać na swoim,
+                lubi się głaskać psa.
+
+                Poezję –
+                tylko co to takiego poezja.
+                Niejedna chwiejna odpowiedź
+                na to pytanie już padła.
+                A ja nie wiem i nie wiem i trzymam się tego
+                jak zbawiennej poręczy."""); //11
+        System.out.println("---------------------------#12----------------------------------------");
+        System.out.println("""
+                *  *  *  *  *  *  *  * ==================================
+                *  *  *  *  *  *  *  * ==================================
+                *  *  *  *  *  *  *  * ==================================
+                *  *  *  *  *  *  *  * ==================================
+                *  *  *  *  *  *  *  * ==================================
+                *  *  *  *  *  *  *  * ==================================
+                =========================================================
+                =========================================================
+                =========================================================
+                =========================================================
+                ========================================================="""); //12
+
+
+    }
+}
